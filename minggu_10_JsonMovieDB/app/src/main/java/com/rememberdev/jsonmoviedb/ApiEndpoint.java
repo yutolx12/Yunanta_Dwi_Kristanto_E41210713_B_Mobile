6d802c18878e85816476a199b3de42034eb3d419
yutolx12 <yunanta86@gmail.com>
2023-05-10 10:54:47 +0700

    add fix minggu 10

---
+++ minggu_10_JsonMovieDB/app/src/main/java/com/rememberdev/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,14 @@
+package com.rememberdev.jsonmoviedb;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=625a2329be780fba57a5f906fb7365eb";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+
+}
